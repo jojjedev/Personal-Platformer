7c5ba68aac063023c06c2579665a0d58404b3c46
Johan Holmberg <c26johho@student.his.se>
2026-09-23 15:29:08 +0200

    Scene and entity updated

--- Entity.cs
+++ Entity.cs
@@ -1,4 +1,5 @@
 ﻿using SFML.Graphics;
+using SFML.System;
 
 namespace PersonalPlatformer;
 
@@ -8,10 +9,23 @@ public class Entity : Scene
     protected readonly Sprite sprite;
     public bool Dead;
 
-    public Entity(string textureName)
+    protected Entity(string textureName)
     {
         this.textureName = textureName;
         sprite = new Sprite();
+    }
+
+    public Vector2f Position
+    {
+        get => sprite.Position;
+        set => sprite.Position = value;
+    }
+
+    public virtual FloatRect Bounds => sprite.GetGlobalBounds();
+
+    public void Create(Scene scene)
+    {
+        sprite.Texture = scene.LoadTexture(textureName);
     }
     public virtual void Update(Scene scene, float dt){}
 
--- Scene.cs
+++ Scene.cs
@@ -1,17 +1,58 @@
-﻿namespace PersonalPlatformer;
+﻿using SFML.Graphics;
+
+namespace PersonalPlatformer;
 
 public class Scene
 {
-    private Dictionary<string, Entity> textures;
+    private Dictionary<string, Texture> textures;
     private List<Entity> entities;
     private string nextScene;
     private string currentScene;
 
+    public Scene()
+    {
+        textures = new Dictionary<string, Texture>();
+        entities = new List<Entity>();
+    }
+    public void Spawn(Entity entity)
+    {
+        entities.Add(entity);
+        entity.Create(this);
+    }
+
+    public Texture LoadTexture(string name)
+    {
+        if (textures.TryGetValue(name, out Texture found))
+        {
+            return found;
+        }
+        string fileName = $"assets/{name}.png";
+        Texture texture = new Texture(fileName);
+        textures.Add(name,texture);
+        return texture;
+    }
     public void UpdateAll(float dt)
     {
+        for (int i = entities.Count; i >= 0; i--) // Om en entity skapar en ny entity i sin update funktion vill
+                                                  // man inte att den nya ska köra sin update i samma frame som den skapades, utan vänta till nästa.
+        {
+            Entity entity = entities[i];
+            entity.Update(this, dt);
+        }
+
         for (int i = 0; i < entities.Count; i++)
         {
-            entities[i].Update(this, dt);
+            Entity entity = entities[i];
+            if (entity.Dead) entities.RemoveAt(i);
+            else i++;
+        }
+    }
+
+    public void RenderAll(RenderTarget target)
+    {
+        foreach (Entity entity in entities)
+        {
+            entity.Render(target);
         }
     }
 }
